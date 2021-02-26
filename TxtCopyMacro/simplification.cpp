ea49adfd14152ac99d7c2addb937e09bebbc6042
scdoit22 <scdoit22@gmail.com>
2021-02-26 21:05:51 +0900

    1

---
+++ TxtCopyMacro/simplification.cpp
@@ -0,0 +1,79 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <regex>
+#include <vector>
+
+class simplification
+{
+    private:
+    char chapter[1];
+    std::regex chapterRegex; // 챕터 찾는 정규표현식
+    std::regex wordRegex;
+    std::vector<std::string> output;
+    const std::string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public:
+    simplification()
+    {
+        chapterRegex = "[A-Z] [가-힣]+( [가-힣]*)*";
+        wordRegex = "•[a-z ]*";
+    }
+    void input()
+    {
+        std::ifstream f;
+        f.open("D:\\Programming\\DB\\XiBasicVoca.txt", std::ios::in);
+
+        if (!f.is_open()) 
+        {
+            std::cout << "파일을 찾을 수 없습니다!" << std::endl;
+        }
+
+        std::string buf;
+        int count = 0;
+        std::string str;
+        std::smatch match;
+        while (true) 
+        {
+            if(f.eof()) break;
+            std::getline(f, buf);
+            str = buf;
+            if(std::regex_search(str, match, wordRegex))
+            {
+                str = buf;
+                output.push_back(match.str());
+                std::cout << match.str() << "\n";
+            }
+            else if(std::regex_search(str, chapterRegex))
+            {
+                str = str[0];
+                str += std::to_string(count);
+                output.push_back(str);
+                std::cout << str << std::endl;;
+                ++count;
+            }
+            
+        }
+        f.close();
+    }
+    // void wordCollect()
+    // {
+    //     chapter[0] += 1;
+    //     chapterRegex = strcat(chapter," [0-9]{2}");
+    //     while(f)
+    //     {   
+    //         f.getline(buf, 100);
+    //          if(std::regex_match(buf, chapterRegex))
+    //         {
+    //             break;
+    //         }
+    //     }
+    //     f.close();
+    // }
+};
+int main(void)
+{
+    simplification si;
+    si.input();
+    return 0;
+}
