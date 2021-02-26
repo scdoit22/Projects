986093390ef5d2f76dc4cf05f5228cebf0214f01
scdoit22 <scdoit22@gmail.com>
2021-02-26 21:09:16 +0900

    1

--- TxtCopyMacro/main.cpp
+++ TxtCopyMacro/main.cpp
@@ -3,45 +3,60 @@
 #include <iostream>
 #include <regex>
 
-
-
-class chapterFind
+class CopyMacro
 {
     private:
     char chapter[1];
     std::regex chapterRegex; // 챕터 찾는 정규표현식
-    
+    std::ifstream f;
+    char buf[100];
+
     public:
-    chapterFind()
+    CopyMacro()
     {
         std::cin >> chapter; //chpater = A(1)~S(19)
         chapterRegex = strcat(chapter, " [0-9]{2}");
+        f.open("D:\\Programming\\DB\\XiBasicVoca.txt", std::ios::in);
+
     }
     void Find()
     {
-        std::ifstream f;
-        f.open("D:\\Programming\\DB\\XiBasicVoca.txt", std::ios::in);
-
-        char buf[100];
-
-        if (!f.is_open()) {
+        if (!f.is_open()) 
+        {
             std::cout << "파일을 찾을 수 없습니다!" << std::endl;
         }
 
-        while (f) {
+        while (f) 
+        {
             f.getline(buf, 100);
             if(std::regex_match(buf, chapterRegex))
             {
                 break;
             }
         }
+        
+    }
+    void wordCollect()
+    {
+        chapter[0] += 1;
+        chapterRegex = strcat(chapter," [0-9]{2}");
+        while(f)
+        {   
+            f.getline(buf, 100);
+             if(std::regex_match(buf, chapterRegex))
+            {
+                break;
+            }
+        }
+        f.close();
     }
 };
 int main(void)
 {
 
-    chapterFind find;
+    CopyMacro cm;
 
-    find.Find();
+    cm.Find();
+    cm.wordCollect();
     return 0;
 }
--- TxtCopyMacro/test.cpp
+++ TxtCopyMacro/test.cpp
@@ -1,18 +1,22 @@
+#include <vector>
 #include <iostream>
 #include <regex>
 #include <string>
 
+
 int main(void)
 {
-    char chapter[1];
-    std::string str = "A 01";    
-    std::regex chapterRegex;
-    std::cin >> chapter; //chpater = A(1)~S(19)
-    chapterRegex = strcat(chapter," [0-9][0-9]");
-    // chapterRegex = "A [0-9][0-9]";
-    if(std::regex_match(str, chapterRegex))
-    {
-        std::cout << "True";
-    }
+    // char chapter[1];
+    // std::string str = "A 01";    
+    // std::regex chapterRegex;
+    // std::cin >> chapter; //chpater = A(1)~S(19)
+    // chapterRegex = strcat(chapter," [0-9][0-9]");
+    // // chapterRegex = "A [0-9][0-9]";
+    // if(std::regex_match(str, chapterRegex))
+    // {
+    //     std::cout << "True";
+    // }
+    std::vector<std::string> v;
+    v.push_back('a');
     return 0;
 }
